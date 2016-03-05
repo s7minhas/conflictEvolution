63c78535b12c50de34ae8c8a9bd9ed3723a01bc6
hermes829 <hermes829@hotmail.com>
2016-03-05 13:34:00 -0800

    ugh...nothing interesting

--- R/Modelling/amenPosterior.R
+++ R/Modelling/amenPosterior.R
@@ -4,15 +4,13 @@ source('~/Research/conflictEvolution/R/setup.R')
 # Load output files into list
 iter=10000 ; toBurn=1001 ; odes=10
 load(file=paste0(pathResults, 'modList.rda'))
+
 binMods = lapply(names(modList), function(mod){ load( paste0(pathResults,mod,'_bin.rda') ) ; return(fit) }) ; names(binMods) = names(modList)
-ordMods = lapply(names(modList), function(mod){ load( paste0(pathResults,mod,'_ord.rda') ) ; return(fit) }); names(ordMods) = names(ordMods)
-
-# Trace plots
 tracePlotsBin = lapply(binMods, getTracePlot)
-tracePlotsOrd = lapply(ordMods, getTracePlot)
-
-# Posterior sumamry stats
 coefDataBin = lapply(1:length(binMods), function(ii){ getCoefData(ii, binMods, modList) } ) %>% do.call('rbind', .)
-coefDataOrd = lapply(1:length(ordMods), function(ii){ getCoefData(ii, ordMods, modList) } ) %>% do.call('rbind', .)
 getCoefPlot( coefDataBin )
+
+ordMods = lapply(names(modList), function(mod){ load( paste0(pathResults,mod,'_ord.rda') ) ; return(fit) }); names(ordMods) = names(ordMods)
+tracePlotsOrd = lapply(ordMods, getTracePlot)
+coefDataOrd = lapply(1:length(ordMods), function(ii){ getCoefData(ii, ordMods, modList) } ) %>% do.call('rbind', .)
 getCoefPlot( coefDataOrd )
