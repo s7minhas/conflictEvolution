3b8ef1c94f062637f25bc24f4218088a70a15446
hermes829 <hermes829@hotmail.com>
2016-03-07 22:38:50 -0800

    cleanup

--- R/Modelling/runAmen.R
+++ R/Modelling/runAmen.R
@@ -29,8 +29,11 @@ dvMatsOrd<-array(unlist(matListCrimeOrd), dim=c(nrow(matListCrimeOrd[[1]]), ncol
 # Transform Y so that events only occur between dto-gov and dto-dto
 govActors = dimnames(dvMats)[[1]][c(1:3,6:9)]
 dtoActors = dimnames(dvMats)[[1]][c(4:5,10:17)]
-# dvMats[govActors,govActors,] = 0
-# dvMatsOrd[govActors,govActors,] = 0
+
+# DV mods
+# pathResults ='~/Dropbox/Research/conflictEvolution/results/all_interactions/'
+dvMats[govActors,govActors,] = 0
+dvMatsOrd[govActors,govActors,] = 0
 
 #load nodal covariates 
 Xnode = allNodal[,,as.character(2007:2013)]
@@ -52,37 +55,31 @@ for(ii in 1:dim(Xnodev2)[3]){
 }
 
 # Set up model specs
-pathResults ='~/Dropbox/Research/conflictEvolution/results/all_interactions/'
 modList = list(
-    null = list(yBin=yBin, yOrd=yOrd, Xnode=NULL, Xdyad=NULL, intM=TRUE, name='null'),
-    net = list(yBin=yBin, yOrd=yOrd, Xnode=Xnodev2[,c('centrality', 'betweeness'),], Xdyad=NULL, intM=TRUE, name='net'),
-    # dtoF = list(yBin=yBin, yOrd=yOrd, Xnode=Xnodev2[,c('dto'),,drop=FALSE], Xdyad=NULL, intM=FALSE, name='dtoF'),
-    # dtoF_net = list(yBin=yBin, yOrd=yOrd, Xnode=Xnodev2[,c('dto', 'centrality', 'betweeness'),,drop=FALSE], Xdyad=NULL, intM=FALSE, name='dtoF_net'),    
-    # dtoF_protest = list(yBin=yBin, yOrd=yOrd, Xnode=Xnodev2[,c('dto','protestLagCount'),], Xdyad=NULL, intM=FALSE, name='dtoF_protest'),    
-    protest = list(yBin=yBin, yOrd=yOrd, Xnode=Xnodev2[,c('protestLagCount'),,drop=FALSE], Xdyad=NULL, intM=TRUE, name='protest'),            
-    protest_net = list(yBin=yBin, yOrd=yOrd, Xnode=Xnodev2[,c('centrality', 'betweeness', 'protestLagCount'),,drop=FALSE], Xdyad=NULL, intM=TRUE, name='protest_net'),
-    protest_net_dto = list(yBin=yBin, yOrd=yOrd, Xnode=Xnodev2[,c('centrality', 'betweeness', 'protestLagCount','dto'),,drop=FALSE], Xdyad=NULL, intM=TRUE, name='protest_net_dto'),
-    dtoF_protest_net = list(yBin=yBin, yOrd=yOrd, Xnode=Xnodev2[,c('dto', 'centrality', 'betweeness', 'protestLagCount'),,drop=FALSE], Xdyad=NULL, intM=FALSE, name='dtoF_protest_net')     
+    net = list(yBin=yBin, yOrd=yOrd, Xnode=Xnodev2[,c('centrality', 'betweeness'),], Xdyad=NULL, name='net'),
+    protest = list(yBin=yBin, yOrd=yOrd, Xnode=Xnodev2[,c('protestLagCount'),,drop=FALSE], Xdyad=NULL, name='protest'),            
+    protest_net = list(yBin=yBin, yOrd=yOrd, Xnode=Xnodev2[,c('centrality', 'betweeness', 'protestLagCount'),,drop=FALSE], Xdyad=NULL, name='protest_net'),
+    protest_net_dto = list(yBin=yBin, yOrd=yOrd, Xnode=Xnodev2[,c('centrality', 'betweeness', 'protestLagCount','dto'),,drop=FALSE], Xdyad=NULL, name='protest_net_dto')
     )
 save(modList, file=paste0(pathResults, 'modList.rda'))
 
-# Run binary mods
+# # Run binary mods
 iter=10000 ; toBurn=1001 ; odes=10
 print('Running binary amen models...')
-cl = makeCluster(6)
+cl = makeCluster(4)
 registerDoParallel(cl)
 foreach(mod = names(modList), .packages=c('amen')) %dopar% {
     fit = ame_rep(Y=modList[[mod]]$'yBin', Xdyad=modList[[mod]]$'Xdyad', modList[[mod]]$'Xnode', model='bin',
-        symmetric=TRUE, intercept=modList[[mod]]$'intM', burn=toBurn, nscan=iter, odens=odes, plot=FALSE, print = FALSE )
+        symmetric=TRUE, burn=toBurn, nscan=iter, odens=odes, plot=FALSE, print = FALSE )
     save(fit, file=paste0(pathResults, modList[[mod]]$'name', '_bin.rda') ) }
 stopCluster(cl)
 
 # Run ordinal mods
 print('Running ordinal amen models...')
-cl = makeCluster(6)
+cl = makeCluster(4)
 registerDoParallel(cl)
 foreach(mod = names(modList), .packages=c('amen')) %dopar% {
     fit = ame_rep(Y=modList[[mod]]$'yOrd', Xdyad=modList[[mod]]$'Xdyad', modList[[mod]]$'Xnode', model='ord',
-        symmetric=TRUE, intercept=modList[[mod]]$'intM', burn=toBurn, nscan=iter, odens=odes, plot=FALSE, print = FALSE )
+        symmetric=TRUE, burn=toBurn, nscan=iter, odens=odes, plot=FALSE, print = FALSE )
     save(fit, file=paste0(pathResults, modList[[mod]]$'name', '_ord.rda')) }
 stopCluster(cl)
