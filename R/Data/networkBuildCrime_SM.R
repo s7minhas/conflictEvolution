338b631f29f2322e12d3320adf5844050e782c7b
cassyld <cassyld@gmail.com>
2016-05-31 10:11:28 -0600

    directory

--- R/Data/networkBuildCrime_SM.R
+++ R/Data/networkBuildCrime_SM.R
@@ -1,6 +1,8 @@
 #################
 # workspace
 if(Sys.info()['user']=='janus829' | Sys.info()['user']=='s7m'){ source('~/Research/conflictEvolution/R/setup.R')  }
+
+if(Sys.info()['user']=='cassydorff' | Sys.info()['user']=='cassydorff'){ source('~/ProjectsGit/conflictEvolution/R/setup.R')  }
 #################
 
 #################
