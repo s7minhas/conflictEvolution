aec4a4c803e31102df0900ce86d3d3ebb891b8d7
s7minhas <s7minhas@gmail.com>
2016-09-21 10:54:44 -0700

    subsetted data

--- R/Data/data0116Explore.R
+++ R/Data/data0116Explore.R
@@ -15,3 +15,21 @@ actors = unique( c( mexicoStories$sender, mexicoStories$target ) )
 actors = data.frame(actor = actors, stringsAsFactors = FALSE)
 write.csv(actors, file=paste0(pathData, 'actorList0116.csv'))
 #################
+
+#################
+actors = read.csv(paste0(pathData, 'actorList0116MS.csv'))
+mexActors = char( actors$X[which(actors$notMexConflict==0)] )
+
+mexicoStoriesSubset = mexicoStories[which(mexicoStories$sender %in% mexActors),]
+mexicoStoriesSubset = mexicoStoriesSubset[which(mexicoStoriesSubset$target %in% mexActors),]
+#################
+
+#################
+mexicoStoriesSubset$year = num(unlist(lapply(strsplit(mexicoStoriesSubset$event_date, '-'), function(x){x[1]})))
+mexicoStoriesSubset = mexicoStoriesSubset[mexicoStoriesSubset$year>2005 & mexicoStoriesSubset$year<2013,]
+#################
+
+#################
+# save revised story db
+save(mexicoStoriesSubset, file=paste0(pathData, 'mexicoVioSqlStories_onlyMexActors_2006_2012.Rda'))
+#################
