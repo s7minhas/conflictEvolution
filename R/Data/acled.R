cb1b6842124409beba3d1b524039f664f28ce74a
cassyld <cassyld@gmail.com>
2016-12-08 07:48:58 -0700

    update rank

--- R/Data/acled.R
+++ R/Data/acled.R
@@ -8,14 +8,18 @@ if(Sys.info()['user']=='maxgallop'){ source('~/Documents/conflictEvolution/R/set
 #################
 
 #load data
-acled<-read.csv(paste0(pathData,"ACLED-Asia-Running-file-January-to-December-2015-V2.csv"), na.strings="")
+acledAsia<-read.csv(paste0(pathData,"ACLED-Asia-Running-file-January-to-December-2015-V2.csv"), na.strings="")
+acledAf<-read.csv(paste0(pathData,"ACLED Version 6 All Africa 1997-2015_csv_dyadic.csv"), na.strings="")
 #################
 
 summ = function(x) {
   total=length(x)
   numNA = sum(is.na(x))
   leftovers = length(x) - numNA # also could this just be sum(!is.na(x))
-  result = c(total=total, numNA = numNA, leftovers = leftovers)
+  #result = c(total=total, numNA = numNA, leftovers = leftovers)
+  result = c(leftovers = leftovers)
   return(result)}
-  summary = tapply(acled$ACTOR2, acled$COUNTRY, summ ) 
-  
+
+summaryAf = tapply(acledAf$ACTOR2, acledAf$COUNTRY, summ ) 
+summaryAf[order(summaryAf, decreasing=TRUE)]
+range(acledAf$YEAR)  
