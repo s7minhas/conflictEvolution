fbc086b0f59810969fb53e60e93f8a96aa058834
s7minhas <s7minhas@gmail.com>
2017-02-07 17:50:42 -0800

    update plots

--- R/Modelling/3b_netPlots.R
+++ R/Modelling/3b_netPlots.R
@@ -6,6 +6,7 @@ if(Sys.info()['user']=='cassydorff' | Sys.info()['user']=='cassydorff'){
 	source('~/ProjectsGit/conflictEvolution/R/setup.R')  }
 if(Sys.info()['user']=='maxgallop'){
 	source('~/Documents/conflictEvolution/R/setup.R')  }
+source(paste0(fPth, 'netPlotHelpers.R'))
 ################
 
 ################
@@ -13,17 +14,30 @@ if(Sys.info()['user']=='maxgallop'){
 load(paste0(pathResults, 'ameResults.rda')) # load AME mod results
 yArr = listToArray(actors=sort(unique(unlist(lapply(yList,rownames)))), 
 	Y=yList, Xdyad=NULL, Xrow=NULL, Xcol=NULL)$Y
-################
+yArrSumm = apply(yArr, c(1,2), sum, na.rm=TRUE)
 
-################
-paramPlot(fitFullSpec$VC[,-ncol(fitFullSpec$VC)])
-gofPlot(fitFullSpec$GOF[,3:4], FALSE)
+# adjust actor names
+vNameKey = getNameKey(yList)
+rownames(yArrSumm) = vNameKey$clean[match(rownames(yArrSumm), vNameKey$dirty)]
+colnames(yArrSumm) = vNameKey$clean[match(colnames(yArrSumm), vNameKey$dirty)]
+rownames(fitFullSpec$U) = vNameKey$clean[match(rownames(fitFullSpec$U), vNameKey$dirty)]
+rownames(fitFullSpec$V) = vNameKey$clean[match(rownames(fitFullSpec$V), vNameKey$dirty)]
 ################
 
 ################
 # multiplicative effects
-yArrSumm = apply(yArr, c(1,2), sum, na.rm=TRUE)
-circplot(Y=yArrSumm, U=fitFullSpec$U, V=fitFullSpec$V, pscale=.7)
-circplot(Y=yArrSumm, U=fitFullSpec$U, V=NULL, pscale=.7)
-circplot(Y=yArrSumm, U=fitFullSpec$V, V=NULL, pscale=.7)
+circplot(Y=yArrSumm, U=fitFullSpec$U, V=fitFullSpec$V,
+	pscale=1, 
+	rcol='black', ccol='black', jitter=.2,
+	lcol='gray80')
+################
+
+################
+uvCols = brewer.pal(11, 'RdBu')[c(11-2, 3)]
+circPlot=ggCirc(
+	Y=yArrSumm, U=fitFullSpec$U, V=fitFullSpec$V,
+	vscale=.6, family="Source Sans Pro Light",
+	lcol='gray90', lsize=.1) +
+	scale_color_manual(values=uvCols)
+ggsave(circPlot, file=paste0(pathGraphics,'circPlot.pdf'), width=10, height=8, device=cairo_pdf)
 ################
