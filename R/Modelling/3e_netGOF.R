1a2bb7dbe70aeaa54e521e3e6922d5193f9cc097
s7minhas <s7.minhas@gmail.com>
2017-02-09 22:36:51 -0800

    off to cd

---
+++ R/Modelling/3e_netGOF.R
@@ -0,0 +1,30 @@
+################
+# workspace
+if(Sys.info()['user']=='janus829' | Sys.info()['user']=='s7m'){
+	source('~/Research/conflictEvolution/R/setup.R')  }
+if(Sys.info()['user']=='cassydorff' | Sys.info()['user']=='cassydorff'){
+	source('~/ProjectsGit/conflictEvolution/R/setup.R')  }
+if(Sys.info()['user']=='maxgallop'){
+	source('~/Documents/conflictEvolution/R/setup.R')  }
+source(paste0(fPth, 'gofPlot2.R'))
+################
+
+################
+# load data
+load(paste0(pathResults, 'ameResults.rda')) # load AME mod results
+################
+
+################
+gof = fitFullSpec$GOF
+# colnames(gof) = c('rowVar','colVar','dVar','tVar')
+varKey = data.frame(dirty=colnames(gof), stringsAsFactors = FALSE)
+varKey$clean = c('Sender variation', 'Receiver variation', 
+	'Dyadic dependency', 'Triadic dependency')
+ggGOF = gofPlot2(gof, FALSE, varKey) +
+	theme(
+		axis.text.x=element_text(family='Source Sans Pro Light'),
+		axis.title.x=element_text(family='Source Sans Pro Light'),
+		strip.text.x = element_text(family="Source Sans Pro Semibold")
+		)
+ggsave(ggGOF, file=paste0(pathGraphics, 'netGOF.pdf'), height=6, width=8, device=cairo_pdf)
+################
