6533d487d05e7abb4ddc5c9d34a984997e2f44c4
s7minhas <s7.minhas@gmail.com>
2017-02-27 16:50:45 -0800

    code for new mods, need to push to server

--- R/Data/1_getNigeriaFrame.R
+++ R/Data/1_getNigeriaFrame.R
@@ -1,8 +1,11 @@
 ################
 # workspace
-if(Sys.info()['user']=='janus829' | Sys.info()['user']=='s7m'){ source('~/Research/conflictEvolution/R/setup.R')  }
-if(Sys.info()['user']=='cassydorff' | Sys.info()['user']=='cassydorff'){ source('~/ProjectsGit/conflictEvolution/R/setup.R')  }
-if(Sys.info()['user']=='maxgallop'){ source('~/Documents/conflictEvolution/R/setup.R')  }
+if(Sys.info()['user']=='janus829' | Sys.info()['user']=='s7m'){
+	source('~/Research/conflictEvolution/R/setup.R')  }
+if(Sys.info()['user']=='cassydorff' | Sys.info()['user']=='cassydorff'){
+	source('~/ProjectsGit/conflictEvolution/R/setup.R')  }
+if(Sys.info()['user']=='maxgallop'){
+	source('~/Documents/conflictEvolution/R/setup.R')  }
 #################
 
 #################
@@ -19,17 +22,22 @@ nData$aa1 = trim(nData$ALLY_ACTOR_1) ; nData$aa2 = trim(nData$ALLY_ACTOR_2)
 ids = c('a1','a2','aa1','aa2')
 
 # replace military forces of nigeria and policies forces of nigiera year ids
-for(id in ids){ nData[,id][which(grepl('Military Forces of Nigeria',nData[,id]))] = 'Military Forces of Nigeria' }
-for(id in ids){ nData[,id][which(grepl('Police Forces of Nigeria',nData[,id]))] = 'Police Forces of Nigeria' }
+for(id in ids){
+	nData[,id][which(grepl('Military Forces of Nigeria',nData[,id]))] = 'Military Forces of Nigeria' }
+for(id in ids){
+	nData[,id][which(grepl('Police Forces of Nigeria',nData[,id]))] = 'Police Forces of Nigeria' }
 
 # nscdc paramilitary arm of nigeria
-for(id in ids){ nData[,id][nData[,id]=='NSCDC: Nigeria Security and Civil Defence Corps'] = 'Military Forces of Nigeria' }
+for(id in ids){
+	nData[,id][nData[,id]=='NSCDC: Nigeria Security and Civil Defence Corps'] = 'Military Forces of Nigeria' }
 
 # create master Oodua Peoples Congress category
-for(id in ids){ nData[,id][nData[,id]=="Mutiny of OPC: Oodua Peoples Congress"] = "OPC: Oodua Peoples Congress" }
+for(id in ids){
+	nData[,id][nData[,id]=="Mutiny of OPC: Oodua Peoples Congress"] = "OPC: Oodua Peoples Congress" }
 
 # create master boko haram category
-for(id in ids){ nData[,id][which(grepl('Boko Haram',nData[,id]))] = 'Boko Haram' }
+for(id in ids){
+	nData[,id][which(grepl('Boko Haram',nData[,id]))] = 'Boko Haram' }
 
 # remove unidentified groups
 for(id in ids[1:2]){ nData = nData[which(!grepl('Unidentified', nData[,id])),] }
