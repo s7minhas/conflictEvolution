c8660e49a7e7e2ce2688572505be16f20c35c448
s7minhas <s7.minhas@gmail.com>
2017-04-09 18:54:46 -0500

    outsamp code

--- R/Modelling/2c_outSampByTime.R
+++ R/Modelling/2c_outSampByTime.R
@@ -14,19 +14,24 @@ source(paste0(fPth, 'binPerfHelpers.R'))
 
 ################
 # load data
+load(paste0(pathData, 'nigeriaMatList_acled_v7.rda')) # loads yList object
 load(paste0(pathResults, 'ameResults.rda'))
 
-# glm data
-y = melt(yList) ; yLag = melt(yList) ; yLag$L1 = char(num(yLag$L1)-1)
-xd = melt(xDyadL)
-xd = cbind(xd[xd$Var3=='govActor',],
-	postBoko=xd$value[xd$Var3=='postBoko'],
-	medianDist=xd$value[xd$Var3=='medianDist']
-	)
-names(xd)[4]='govActor'
+# construct dyadic design array from list data
+yrs = char(2000:2016) ; yList = yList[yrs]
+xDyadL = designArrays$base$dyadCovar ; dyadVars = dimnames(xDyadL[[1]])[[3]]
+xRowL = designArrays$base$senCovar ; senVars = paste0(dimnames(xRowL[[1]])[[2]],'.row')
+xColL = designArrays$base$recCovar ; recVars = paste0(dimnames(xColL[[1]])[[2]],'.col')
+
+# break out of lists/arrays
+y = melt(yList) ; yLag = melt(yList); yLag$L1 = char(num(yLag$L1)-1)
+xd = dcast(melt(xDyadL), Var1 + Var2 + L1 ~ Var3)
+xd = xd[order(xd$L1, xd$Var2, xd$Var1),]
 xr = dcast(melt(xRowL), Var1 + L1 ~ Var2)
 xc = dcast(melt(xColL), Var1 + L1 ~ Var2)
-glmData = cbind(y,xd[,c('govActor','postBoko','medianDist')])
+
+# combine into df
+glmData = cbind(y,xd)
 for(v in names(xc)[3:ncol(xc)] ){
 	glmData$tmp = xr[,v][match(paste0(glmData$Var1,glmData$L1),
 		paste0(xr$Var1,xr$L1))]
@@ -42,89 +47,88 @@ glmData$lagDV[is.na(glmData$lagDV)] = 0
 ################
 
 ################
+# get glm info
+glmPredInfo = function(fitIn_glm, glmData_Out){
+	glm_Pred = data.matrix(cbind(1, glmData_Out[,names(coef(fitIn_glm))[-1]])) %*% coef(fitIn_glm)
+	glm_Prob = 1/(1+exp(-glm_Pred))
+	outPerf_glm = data.frame(glmData_Out[,1:3], pred=glm_Prob)
+	aucROC_glm=getAUC(outPerf_glm$pred, outPerf_glm$value)
+	aucPR_glm=auc_pr(outPerf_glm$value, outPerf_glm$pred)
+	return(list(glm_Pred=glm_Pred, aucROC_glm=aucROC_glm, aucPR=aucPR_glm)) }
+################
+
+################
 # cross val by time split
+loadPkg(c('parallel','foreach'))
+cores=5 ; cl=makeCluster(cores) ; registerDoParallel(cl)
+ame_glm_outSampTime = foreach(dropFromEnd = 1:5, .packages=c('amen')) %dopar% {
 
-# organize data
-dropFromEnd=1
-y_In = yList[ 1:(length(yList)-dropFromEnd) ]
-# xDyadL_In = xDyadL_noDist[ 1:(length(yList)-dropFromEnd) ]
-xDyadL_In = xDyadL[ 1:(length(yList)-dropFromEnd) ]
-xRowL_In = xRowL[ 1:(length(yList)-dropFromEnd) ]
-xColL_In = xColL[ 1:(length(yList)-dropFromEnd) ]
-glmData_In = glmData[which(glmData$L1 %in% names(yList)[1:(length(yList)-dropFromEnd)]), ]
+	# 
+	modLagDV = formula('value ~ lagDV')
+	modSpecFull = formula( paste0(paste0('value ~ '), 
+		paste(c(dyadVars, senVars, recVars), collapse=' + ') ) )
+	modSpecFullLagDV = formula( paste0(paste0('value ~ lagDV + '), 
+		paste(c(dyadVars, senVars, recVars), collapse=' + ') ) )
 
-y_Out = yList[[ length(yList) - (dropFromEnd - 1) ]]
-# xDyadL_Out = xDyadL_noDist[[ length(yList) - (dropFromEnd - 1) ]]
-xDyadL_Out = xDyadL[[ length(yList) - (dropFromEnd - 1) ]]
-xRowL_Out = xRowL[[ length(yList) - (dropFromEnd - 1) ]]
-xColL_Out = xColL[[ length(yList) - (dropFromEnd - 1) ]]
-glmData_Out = glmData[which(glmData$L1 %in% names(yList)[length(yList) - (dropFromEnd - 1)]), ]
+	# leave out some number of period
+	y_In = yList[ 1:(length(yList)-dropFromEnd) ]
+	xDyadL_In = xDyadL[ 1:(length(yList)-dropFromEnd) ]
+	xRowL_In = xRowL[ 1:(length(yList)-dropFromEnd) ]
+	xColL_In = xColL[ 1:(length(yList)-dropFromEnd) ]
+	glmData_In = glmData[which(glmData$L1 %in% names(yList)[1:(length(yList)-dropFromEnd)]), ]
 
-# # glm Formulas
-# # run with lag dv
-# glmOutSamp_wLagDV=glmOutSamp( glmForm=formula(value ~ lagDV) )
+	y_Out = yList[[ length(yList) - (dropFromEnd - 1) ]]
+	xDyadL_Out = xDyadL[[ length(yList) - (dropFromEnd - 1) ]]
+	xRowL_Out = xRowL[[ length(yList) - (dropFromEnd - 1) ]]
+	xColL_Out = xColL[[ length(yList) - (dropFromEnd - 1) ]]
+	glmData_Out = glmData[which(glmData$L1 %in% names(yList)[length(yList) - (dropFromEnd - 1)]), ]
 
-# # run with ame full spec
-# glmOutSamp_wFullSpec=glmOutSamp(
-# 	glmForm=formula(value ~ govActor + postBoko + medianDist + 
-# 	rioProContra.row + vioCivEvents.row +
-# 	rioProContra.col + vioCivEvents.col) )
+	# run GLM
+	glmSpecFull = glmPredInfo(glm(modSpecFull, data=glmData_In, family='binomial'), glmData_Out)
+	glmSpecFullLagDV = glmPredInfo(glm(modSpecFullLagDV, data=glmData_In, family='binomial'), glmData_Out)
 
-# # ame full spec + lag DV
-# glmOutSamp_wFullSpecLagDV=glmOutSamp(
-# 	glmForm=formula(value ~ lagDV + govActor + postBoko + medianDist + 
-# 	rioProContra.row + vioCivEvents.row +
-# 	rioProContra.col + vioCivEvents.col) )
+	##########
+	# run AME base mod
+	startVals = ameFits$base$startVals
+	startVals$Z = startVals$Z[,,-dim( startVals$Z )[3] ]
+	fitIn_ame = ame_repL(
+		Y=y_In, Xdyad=xDyadL_In, Xrow=xRowL_In, Xcol=xColL_In,
+		symmetric=FALSE, rvar=TRUE, cvar=TRUE, R=2, 
+		model='bin', intercept=TRUE, seed=6886,
+		burn=100000, nscan=500000, odens=25,
+		plot=FALSE, gof=TRUE, periodicSave=FALSE,
+		startVals=startVals
+		) 
 
-# run GLM
-# glmForm=formula(value ~ lagDV + govActor + postBoko +
-glmForm=formula(value ~ lagDV + govActor + postBoko + medianDist + 
-	rioProContra.row + vioCivEvents.row +
-	rioProContra.col + vioCivEvents.col) 
-fitIn_glm = glm(glmForm, data=glmData_In, family='binomial')
+	# org AME results
+	beta = apply(fitIn_ame$BETA,2,mean)
+	a = fitIn_ame$APM[rownames(y_Out)]
+	b = fitIn_ame$BPM[rownames(y_Out)]
+	UVPM = fitIn_ame$UVPM[rownames(y_Out),rownames(y_Out)]
 
-# org glm results
-glm_Pred = data.matrix(cbind(1, glmData_Out[,names(coef(fitIn_glm))[-1]])) %*% coef(fitIn_glm)
-glm_Prob = 1/(1+exp(-glm_Pred))
-outPerf_glm = data.frame(glmData_Out[,1:3], pred=glm_Prob)
-aucROC_glm=getAUC(outPerf_glm$pred, outPerf_glm$value)
-aucPR_glm=auc_pr(outPerf_glm$value, outPerf_glm$pred)
+	# ame design array for out sample
+	x_Out = array(1, 
+		dim=c(  nrow(y_Out),nrow(y_Out), length(beta) ),
+		dimnames=list( rownames(y_Out), rownames(y_Out), names(beta) ) )
+	x_Out[,,2:dim(x_Out)[3]] = design_array_listwisedel(xRowL_Out, xColL_Out, xDyadL_Out, TRUE, nrow(y_Out))
 
-# run AME mods
-# startVals = fitFullSpec_noDist$startVals
-startVals = fitFullSpec$startVals
-startVals$Z = startVals$Z[,,-dim( startVals$Z )[3] ]
-fitIn_ame = ame_repL(
-	Y=y_In, Xdyad=xDyadL_In, Xrow=xRowL_In, Xcol=xColL_In,
-	symmetric=FALSE, rvar=TRUE, cvar=TRUE, R=2, 
-	model='bin', intercept=TRUE, seed=6886,
-	burn=5, nscan=10, odens=1, 
-	plot=FALSE, gof=TRUE, periodicSave=FALSE,
-	startVals=startVals
-	) 
+	# run calcs
+	ez_Out = Xbeta(x_Out, beta) + outer(a,b,'+') + UVPM
+	yHat_Out = 1/(1+exp(-ez_Out))
+	outPerf = data.frame(cbind(reshape2::melt(y_Out), pred=reshape2::melt(yHat_Out)[,3]))
+	outPerf = outPerf[outPerf$Var1 != outPerf$Var2,]
+	aucROC=getAUC(outPerf$pred, outPerf$value)
+	aucPR=auc_pr(outPerf$value, outPerf$pred)
+	ameBase = list(outPerf, aucROC, aucPR)
 
-# org AME results
-beta = apply(fitIn_ame$BETA,2,mean)
-a = fitIn_ame$APM[rownames(y_Out)]
-b = fitIn_ame$BPM[rownames(y_Out)]
-UVPM = fitIn_ame$UVPM[rownames(y_Out),rownames(y_Out)]
+	return(list(glmSpecFull, glmSpecFullLagDV, ameBase))
+}
+################
 
-x_Out = array(1, 
-	dim=c(  nrow(y_Out),nrow(y_Out), length(beta) ),
-	dimnames=list( rownames(y_Out), rownames(y_Out), names(beta) ) )
-x_Out[,,2:dim(x_Out)[3]] = design_array_listwisedel(xRowL_Out, xColL_Out, xDyadL_Out, TRUE, nrow(y_Out))
-
-ez_Out = Xbeta(x_Out, beta) + outer(a,b,'+') + UVPM
-yHat_Out = 1/(1+exp(-ez_Out))
-outPerf = data.frame(cbind(reshape2::melt(y_Out), pred=reshape2::melt(yHat_Out)[,3]))
-outPerf = outPerf[outPerf$Var1 != outPerf$Var2,]
-
-aucROC=getAUC(outPerf$pred, outPerf$value)
-aucPR=auc_pr(outPerf$value, outPerf$pred)
-
-aucROC_glm
-aucPR_glm
-
-aucROC
-aucPR
+################
+# save
+save(
+	ame_glm_outSampTime, 
+	file=paste0(pathResults, 'ameCrossValResults.rda')
+	)
 ################
