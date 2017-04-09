e8f721b6c32115d5d7f3d4a8a47086afbf2bf694
hermes829 <hermes829@gmail.com>
2017-04-09 17:24:47 -0500

    lose a lot of data with same region coding

--- R/Data/3_getExoVars.R
+++ R/Data/3_getExoVars.R
@@ -19,8 +19,7 @@ load(paste0(pathData,"nigeriaMatList_acled_v7.rda")) # adds object named yList
 # violent civilians by year
 loadPkg('doBy') ; nData$x = 1
 vioContraCiv = summaryBy(x+FATALITIES~YEAR+a1,
-	data=nData[nData$EVENT_TYPE=='Violence against civilians',],
-	FUN=sum)
+	data=nData[nData$EVENT_TYPE=='Violence against civilians',], FUN=sum)
 vioContraCiv$YEAR = vioContraCiv$YEAR + 1 # use lagged x
 
 rioData = nData[nData$EVENT_TYPE=='Riots/Protests',c('x','YEAR','a1','a2','aa2')]
@@ -33,14 +32,12 @@ names(tmp2) = names(tmp1) ; rioData=rbind(tmp1,tmp2)
 riotData = rioData[rioData$riots == 1,]
 protData = rioData[rioData$protests == 1,]
 
-rioContraActor = summaryBy(x~YEAR+a2,
-	data=riotData,
-	FUN=sum)
+rioContraActor = summaryBy(x~YEAR+a2, 
+	data=riotData, FUN=sum)
 rioContraActor$YEAR = rioContraActor$YEAR + 1 # use lagged x
 
-protContraActor = summaryBy(x~YEAR+a2,
-                            data=protData,
-                            FUN=sum)
+protContraActor = summaryBy(x~YEAR+a2, 
+	data=protData, FUN=sum)
 protContraActor$YEAR = protContraActor$YEAR + 1 # use lagged x
 #################
 
@@ -76,6 +73,19 @@ xNodeL = lapply(1:length(yList), function(t){
 	protSlice=protSlice[which(protSlice$a2 %in% rownames(nodeMat)),]
 	nodeMat[protSlice$a2,'protestsAgainst'] = protSlice$x.sum
 	
+	# add in combo
+	nodeMat = cbind(nodeMat, riotsProtestsAgainst=(nodeMat[,'riotsAgainst'] + nodeMat[,'protestsAgainst']))
+
+	# add in concatenations with vig militia
+	if('Vigilante Militia (Nigeria)' %in% rownames(y)){
+		vigActions = y['Vigilante Militia (Nigeria)',]
+	} else { vigActions = 0 }
+	nodeMat = cbind(nodeMat, 
+		riotsAgainst_inclVig=(nodeMat[,'riotsAgainst'] + vigActions),
+		protestsAgainst_inclVig=(nodeMat[,'protestsAgainst'] + vigActions),
+		riotsProtestsAgainst_inclVig=(nodeMat[,'riotsProtestsAgainst'] + vigActions)
+		)
+
 	# 
 	return(nodeMat)
 }) ; names(xNodeL) = names(yList)
