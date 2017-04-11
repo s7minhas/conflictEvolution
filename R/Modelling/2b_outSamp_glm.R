834be71f2da2e62e6cbc1cd68b5e6d5b1c13314d
s7minhas <s7minhas@gmail.com>
2017-04-10 17:59:30 -0700

    add recip measure to glm mods

--- R/Modelling/2b_outSamp_glm.R
+++ R/Modelling/2b_outSamp_glm.R
@@ -49,6 +49,7 @@ glmOutSamp = function(glmForm){
 	# melt into glm format
 	yCrossValTrain = lapply(yCrossValTrain, function(y){
 		y = melt(y) ; yLag = melt(yList); yLag$L1 = char(num(yLag$L1)-1)
+		yLagRecip = melt(lapply(yList,function(y){t(y)})); yLagRecip$L1 = char(num(yLagRecip$L1)-1)
 		xd = dcast(melt(xDyadL), Var1 + Var2 + L1 ~ Var3)
 		xd = xd[order(xd$L1, xd$Var2, xd$Var1),]
 		xr = dcast(melt(xRowL), Var1 + L1 ~ Var2)
@@ -62,10 +63,17 @@ glmOutSamp = function(glmForm){
 				paste0(xc$Var1,xc$L1))]
 			names(glmData)[ncol(glmData)] = paste0(v, '.col') }
 		glmData = glmData[which(glmData$Var1!=glmData$Var2),]
+
 		glmData$lagDV = yLag$value[match(
 			paste0(glmData$Var1,glmData$Var2,glmData$L1), 
 			paste0(yLag$Var1,yLag$Var2,yLag$L1))]
 		glmData$lagDV[is.na(glmData$lagDV)] = 0
+
+		glmData$lagRecip = yLagRecip$value[match(
+			paste0(glmData$Var1,glmData$Var2,glmData$L1), 
+			paste0(yLagRecip$Var1,yLagRecip$Var2,yLagRecip$L1))]
+		glmData$lagRecip[is.na(glmData$lagRecip)] = 0		
+
 		return(glmData)	
 	})
 
@@ -124,12 +132,12 @@ glmOutSamp = function(glmForm){
 
 ################
 # run with lag dv
-glmOutSamp_wLagDV=glmOutSamp( glmForm=formula(value ~ lagDV) )
+glmOutSamp_wLagDV=glmOutSamp( glmForm=formula(value ~ lagDV + lagRecip) )
 
 # mod specs
 modSpecFull = formula( paste0(paste0('value ~ '), 
 	paste(c(dyadVars, senVars, recVars), collapse=' + ') ) )
-modSpecFullLagDV = formula( paste0(paste0('value ~ lagDV + '), 
+modSpecFullLagDV = formula( paste0(paste0('value ~ lagDV + lagRecip + '), 
 	paste(c(dyadVars, senVars, recVars), collapse=' + ') ) )
 
 # run with ame full spec
