237257469bb215c0d20d0dd7675ad2fb05d53eec
hermes829 <hermes829@gmail.com>
2017-04-12 09:55:53 -0700

    cleanup outsamp code

--- R/Modelling/3c_perfPlots.R
+++ R/Modelling/3c_perfPlots.R
@@ -15,10 +15,9 @@ predDfs = list(
 	cbind(glmOutSamp_wLagDV$outPerf,model='GLM (Lag DV)'), 
 	cbind(glmOutSamp_wFullSpec$outPerf,model='GLM (Covars)'), 
 	cbind(glmOutSamp_wFullSpecLagDV$outPerf,model='GLM (Lag DV + Covars)'),
-	cbind(ameOutSamp_NULL$outPerf,model='AME (NULL)'), 
 	cbind(ameOutSamp_wFullSpec$outPerf,model='AME (Covars)')
 	)
-names(predDfs) = c('GLM (Lag DV)', 'GLM (Covars)', 'GLM (Lag DV + Covars)', 'AME (NULL)', 'AME (Covars)')
+names(predDfs) = c('GLM (Lag DV)', 'GLM (Covars)', 'GLM (Lag DV + Covars)', 'AME (Covars)')
 
 # tabular data
 aucSumm=do.call('rbind', lapply(predDfs,function(x){
@@ -42,26 +41,25 @@ rocPrData=do.call('rbind',
 # plotting
 
 # model col/lty
-ggCols = brewer.pal(length(levels(rocData$model)), 'Set1')
-ggLty = c('dashed', 'dotted', 'dotdash', 'twodash', 'solid')
+ggCols = brewer.pal(length(levels(rocData$model))+1, 'Set1')[-4]
+ggLty = c('dashed', 'dotted', 'dotdash', 'twodash', 'solid')[-4]
 
 # Separation plots
 loadPkg(c('png','grid'))
 sepPngList = lapply(1:length(predDfs), function(ii){
 	fSepPath = paste0(pathGraphics,'sep_',names(predDfs)[ii],'_outSample.png')
 	# save as pngs for potential use outside of roc
-	# tmp = data.frame(act=predDfs[[ii]]$actual, proba=predDfs[[ii]]$'pred')
-	# ggSep(actual=tmp$act, proba=tmp$proba, 
-	# 	color=ggCols[ii], lty=ggLty[ii], fPath=fSepPath, save=TRUE )
+	tmp = data.frame(act=predDfs[[ii]]$actual, proba=predDfs[[ii]]$'pred')
+	ggSep(actual=tmp$act, proba=tmp$proba, 
+		color=ggCols[ii], lty=ggLty[ii], fPath=fSepPath, save=TRUE )
 	sepG = rasterGrob(readPNG(fSepPath), interpolate=TRUE)
 	return(sepG)
 })
 
 # get rid of null model
-rocData = rocData[rocData$model!='AME (NULL)',] ; rocData$model = factor(rocData$model, levels=levels(rocData$model)[-4])
-rocPrData = rocPrData[rocPrData$model!='AME (NULL)',] ; rocPrData$model = factor(rocPrData$model, levels=levels(rocPrData$model)[-4])
-rownames(aucSumm)[3]="GLM (Lag DV\n  + Covars)"
-ggCols = ggCols[-4] ; ggLty = ggLty[-4] ; sepPngList = sepPngList[-4] ; aucSumm = aucSumm[-2,]
+rocData$model = factor(rocData$model, levels=levels(rocData$model))
+rocPrData$model = factor(rocPrData$model, levels=levels(rocPrData$model))
+rownames(aucSumm)[2]="GLM (Lag DV\n  + Covars)"
 
 tmp = rocPlot(rocData, linetypes=ggLty, colorManual=ggCols)+guides(linetype = FALSE, color = FALSE) ; yLo = -.04 ; yHi = .14
 for(ii in 1:length(sepPngList)){
@@ -81,4 +79,25 @@ tmp=rocPlot(rocPrData, type='pr', legText=12, legPos=c(.25,.35), legSpace=2, lin
 		label=rev(apply(aucSumm, 1, function(x){paste(x, collapse='     ')})),
 		family='Source Sans Pro Light')
 ggsave(tmp, file=paste0(pathGraphics, 'rocPr_outSample.pdf'), width=5, height=5, device=cairo_pdf)
+################################################
+
+################################################
+# add forecast perf plots
+load(paste0(pathResults, 'ameForecastResults.rda')) # ame_glm_outSampTime
+aucSumm = melt( lapply(ame_glm_outSampTime, function(pdMod){
+	lapply(pdMod, function(stats){ stats[2:3] }) }) )
+ggplot(aucSumm, aes(x=L1, y=value, color=L2, group=L2)) + facet_wrap(~L3) + geom_line() + geom_point()
+
+dropFromEnd = 1
+predList = melt(lapply(ame_glm_outSampTime, function(pdMod){
+	lapply(pdMod, function(stats){ stats[1] }) }))
+predDF = predList[
+	predList$L1==paste0('last ',dropFromEnd,' pd excluded'),
+	-which(names(predList) %in% c('Var1','Var2','L3','variable'))]
+
+load(paste0(pathData, 'nigeriaMatList_acled_v7.rda')) # loads yList object
+yrs = char(2000:2016) ; yList = yList[yrs]
+y_Out = yList[[ length(yList) - (dropFromEnd - 1) ]]
+act = reshape2::melt(y_Out) ; act = act[act$Var1 != act$Var2,]
+predDF$value[predDF$L2=='ame']
 ################################################
