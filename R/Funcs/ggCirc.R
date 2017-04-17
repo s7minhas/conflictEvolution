e3d791b07bb913264f9e9d8e3bfa6c23f2199d20
hermes829 <hermes829@gmail.com>
2017-04-16 23:55:58 -0700

    intro changes, and some notes of todos

--- R/Funcs/ggCirc.R
+++ R/Funcs/ggCirc.R
@@ -3,11 +3,14 @@ loadPkg('ggrepel')
 ggCirc = function(
 	Y, U=NULL, V=NULL, row.names=rownames(Y), col.names=colnames(Y),
 	vscale=.8, prange=c(2,5), lcol='gray85', ltype='dotted', lsize=.5,
-	force=1, maxIter = 3e3, removeIsolates=TRUE,
+	force=1, maxIter = 3e3, removeIsolates=TRUE, uLabel='U', vLabel='V',
 	showActLinks=TRUE, geomLabel=TRUE, geomText=FALSE, geomPoint=TRUE, ...
 	){
 	
-	if (is.null(U)) {
+	#
+	vLogic = is.null(V) ; uLogic = is.null(U)
+
+	if (uLogic) {
 	    a <- rowMeans(Y, na.rm = TRUE)
 	    b <- colMeans(Y, na.rm = TRUE)
 	    Y0 <- Y
@@ -32,8 +35,8 @@ ggCirc = function(
 	        v <- u
 	    }
 	}
-	if (!is.null(U)) {
-	    if (is.null(V)) {
+	if (!uLogic) {
+	    if (vLogic) {
 	        V <- U
 	        vscale <- 1
 	    }
@@ -55,18 +58,19 @@ ggCirc = function(
 	uG$tPch = uG$tPch
 	
 	# add v if supplied
-	if(!is.null(V)){
+	if(!vLogic){
 		vG = data.frame(v*1.2)
 		vG$actor = rownames(Y)
 		vG$tPch = 0 ; vG$tPch[csum>0] = (mv[csum>0])^3
 		if(removeIsolates){ vG = vG[vG$tPch>0,] }
 		vG$tPch = vG$tPch
 		
-		uG$eff = 'u' ; vG$eff = 'v'
+		uG$eff = uLabel ; vG$eff = vLabel
 		uG = rbind(uG, vG)		
+		uG$eff = factor(uG$eff, levels=c(uLabel,vLabel))
 		ggCirc = ggplot(uG, aes(x=X1, y=X2,color=eff))
 	}
-	if(is.null(V)){
+	if(vLogic){
 		ggCirc = ggplot(uG, aes(x=X1, y=X2))
 	}
 	
