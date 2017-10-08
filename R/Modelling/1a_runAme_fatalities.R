8fd5382187744d91b2f0269796e424a5090515cc
s7minhas <s7.minhas@gmail.com>
2017-10-08 17:31:34 -0400

    appendix items

--- R/Modelling/1a_runAme_fatalities.R
+++ R/Modelling/1a_runAme_fatalities.R
@@ -56,3 +56,19 @@ save(
 	file=paste0(pathResults, 'ameResults_fatalities.rda')
 	)
 ################
+
+################
+#
+source(paste0(fPth, 'paramPlot2.R'))
+
+# quick trace plot
+mcmcData = ameFits$base$BETA
+varKey = data.frame(dirty=colnames(mcmcData),stringsAsFactors=FALSE)
+varKey$clean = c(
+	'Intercept',
+	'Riots/Protests Against (Sender)', 'Civilian Attacks (Sender)', 'Geographic Spread (Sender)',
+	'Riots/Protests Against (Receiver)', 'Civilian Attacks (Receiver)', 'Geographic Spread (Receiver)',
+	'Gov-Gov Actors','Post-Boko Haram', 'Election Year', 'Neighborhood Conflict')
+varKey = varKey[c(9,2,5,3,6,4,7,10,11,8,1),]
+ggsave(paramPlot2(mcmcData, varKey), file=paste0(pathGraphics, 'betaTrace_fatalities.pdf'), width=8,height=9)
+################
