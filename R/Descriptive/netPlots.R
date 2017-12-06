080287902619506a6d3357f4e995719c2f751169
s7minhas <s7.minhas@gmail.com>
2017-12-06 12:02:50 -0500

    update plots for grant

--- R/Descriptive/netPlots.R
+++ R/Descriptive/netPlots.R
@@ -68,6 +68,18 @@ dev.off() ; system(paste('pdfcrop',fName,fName, sep=' '))
 ################
 
 ################
+# plot by full, pre and post bh periods
+fName = paste0(pathGraphics, 'nigeria_2000_2016_grant.pdf') ; pdf(file=fName, width=13,height=9)
+vCol = 'gray95' ; vLabCol = 'gray30'
+plotGraph(gArrSum, gArrPos, 
+	vShape='circle', vLabCex=gArrSum$labSize+.3, 
+	vLabCol=vLabCol, vertex.label.font=1, vertex.size=gArrSum$vSize, 
+	vFrameCol=vCol, vCol=vCol, vertex.label.family="Helvetica")
+title('Nigerian Intra-State Conflict\n(2000-2016)', family='Helvetica', adj=1, line=-4)	
+dev.off() ; system(paste('pdfcrop',fName,fName, sep=' '))
+################
+
+################
 # plot by fulani
 actorsFulani = c(
 	names(which(yArrSum['Fulani\nMilitia',]==1)),
@@ -79,8 +91,8 @@ tmpFulani=getGraphfromMat(mat=yArrSumFulani)
 gArrSumFulani=tmpFulani$g ; gArrPosFulani=tmpFulani$nodePos ; rm(tmpFulani)
 
 fName = paste0(pathGraphics, 'fulani_2000_2016.pdf') ; pdf(file=fName, width=13,height=9)
-vCol = ifelse(names(V(gArrSumFulani)) %in% govActors, 'gray30', 'gray95')
-vLabCol = ifelse(names(V(gArrSumFulani)) %in% govActors, 'white', 'gray30')
+vCol = ifelse(names(V(gArrSumFulani)) == 'Fulani\nMilitia', 'gray30', 'gray95')
+vLabCol = ifelse(names(V(gArrSumFulani)) == 'Fulani\nMilitia', 'white', 'gray30')
 plotGraph(gArrSumFulani, gArrPosFulani, 
 	vShape='circle', vLabCex=gArrSumFulani$labSize+.3, 
 	vLabCol=vLabCol, vertex.label.font=1, vertex.size=gArrSumFulani$vSize, 
