f74554e5385e994a14aa7fe335ae791e5ec8ce58
s7minhas <s7.minhas@gmail.com>
2018-06-28 00:43:14 -0400

    ame time plot

--- R/Modelling/revAppendix/runAme_byYear.R
+++ R/Modelling/revAppendix/runAme_byYear.R
@@ -8,6 +8,9 @@ if(!'amen' %in% installed.packages()[,1]){
 	loadPkg('devtools')
 	devtools::install_github('s7minhas/amen') }
 library(amen)
+
+#
+source(paste0(fPth, 'postHelpers.R'))
 ################
 
 ################
@@ -29,10 +32,7 @@ designArrays = list(
 	NULL=list(senCovar=NULL, recCovar=NULL, dyadCovar=NULL), 
 	base=list(
 		senCovar=subListArray(xNodeL, c('riotsProtestsAgainst', 'vioCivEvents', 'groupSpread'), 2),
-		recCovar=subListArray(xNodeL, c('riotsProtestsAgainst', 'vioCivEvents', 'groupSpread'), 2),
-		dyadCovar=subListArray(xDyadL, c('govActor','elecYear','ngbrConfCount'), 3)
-		# c('govActor', 'postBoko', 'elecYear', 'ngbrConfCount')
-		# elecs = char(c(2003,2007,2011,2015))
+		recCovar=subListArray(xNodeL, c('riotsProtestsAgainst', 'vioCivEvents', 'groupSpread'), 2)
 		)
 	)
 
@@ -43,25 +43,22 @@ xrList = designArrays[[2]]$recCovar
 
 # break up into smaller pds
 pds = list(
-	2000:2008, 
-	2009:2016)
-# pds = list(
-# 	2000:2005, 2006:2011,
-# 	2012:2016 )
+	2000:2002, 2003:2005, 
+	2006:2008, 2009:2011,
+	2012:2014, 2015:2016
+	)
 pds = lapply(pds, char)
 
 # parallelize model run
-# if(!file.exists(paste0(pathResults, 'revAppendix/ameResults_byYear.rda'))){
+if(!file.exists(paste0(pathResults, 'revAppendix/ameResults_byYear.rda'))){
 	loadPkg(c('parallel','foreach'))
-	cores=length(pds) ; cl=makeCluster(cores) ; registerDoParallel(cl)
+	cores=min(length(pds),5) ; cl=makeCluster(cores) ; registerDoParallel(cl)
 	ameFits = foreach(i = 1:length(pds), .packages=c('amen')) %dopar% {
 
-	# set up data
 		fit = ame_repL(
-			Y=yList[ pds[[i]] ], Xdyad=xdList[ pds[[i]] ],
+			Y=yList[ pds[[i]] ], 
 			Xrow=xsList[ pds[[i]] ], Xcol=xrList[ pds[[i]] ],
-			symmetric=FALSE, rvar=TRUE, cvar=TRUE, R=2, 
-			model='bin', intercept=TRUE, seed=6886,
+			model='bin', intercept=TRUE, seed=6886, 
 			plot=FALSE, gof=TRUE, periodicSave=FALSE )
 		return(fit)
 	} ; stopCluster(cl)
@@ -71,7 +68,7 @@ pds = lapply(pds, char)
 		ameFits, 
 		file=paste0(pathResults, 'revAppendix/ameResults_byYear.rda')
 		)
-# }
+}
 load(paste0(pathResults, 'revAppendix/ameResults_byYear.rda'))
 ################
 
@@ -83,9 +80,13 @@ betaList = lapply(ameFits, function(x){x$'BETA'})
 varKey = data.frame(dirty=colnames(ameFits[[1]]$BETA),stringsAsFactors=FALSE)
 varKey$clean = c(
 	'Intercept',
-	'Riots/Protests Against (Sender)', 'Civilian Attacks (Sender)', 'Geographic Spread (Sender)',
-	'Riots/Protests Against (Receiver)', 'Civilian Attacks (Receiver)', 'Geographic Spread (Receiver)',
-	'Gov-Gov Actors', 'Election Year', 'Neighborhood Conflict')
+	'Riots/Protests$_{i,t-1}$', 
+	'Violent Events Against Civilians$_{i,t-1}$', 
+	'Geographic Spread$_{i,t-1}$',
+	'Riots/Protests$_{j,t-1}$', 
+	'Violent Events Against Civilians$_{j,t-1}$', 
+	'Geographic Spread$_{j,t-1}$'
+	)
 ################
 
 ################
@@ -96,17 +97,8 @@ for(i in 1:length(pds)){
 		lapply(yList[ pds[[i]] ], function(x){
 			diag(x) = NA ; return(c(x)) })), na.rm=TRUE)
 	sd_xs = apply(do.call('rbind',xsList[ pds[[i]] ]), 2, sd)
-	sd_xr = apply(do.call('rbind',xrList[ pds[[i]] ]), 2, sd)
-	sd_xd1 = sd(unlist(
-		lapply(xdList[ pds[[i]] ], function(x){
-			x=x[,,1]; diag(x) = NA ; return(c(x)) })), na.rm=TRUE)
-	sd_xd2 = sd(unlist(
-		lapply(xdList[ pds[[i]] ], function(x){
-			x=x[,,2]; diag(x) = NA ; return(c(x)) })), na.rm=TRUE)	
-	sd_xd3 = sd(unlist(
-		lapply(xdList[ pds[[i]] ], function(x){
-			x=x[,,3]; diag(x) = NA ; return(c(x)) })), na.rm=TRUE)		
-	sdX = c(1, sd_xs, sd_xr, sd_xd1, sd_xd2, sd_xd3)
+	sd_xr = apply(do.call('rbind',xrList[ pds[[i]] ]), 2, sd)	
+	sdX = c(.1, sd_xs, sd_xr)
 
 	# sdz betas
 	tmp=ameFits[[i]]$BETA
@@ -116,11 +108,8 @@ for(i in 1:length(pds)){
 
 ################
 # time labels
-labs=1:length(pds)
-# labs = c(
-# 	'2000-2005', '2006-2011',
-# 	'2012-2016'
-# 	)
+labs = unlist(lapply(pds, function(x){
+  paste(x[1], x[length(x)],sep='-') }))
 
 # summarize param estimates
 paramTime = lapply(1:length(ameFits), function(i){
@@ -137,15 +126,27 @@ paramTime = lapply(1:length(ameFits), function(i){
 
 ################	
 # viz
-ggplot(paramTime, aes(x=factor(pd), y=mean)) +
+facet_labeller = function(string){ TeX(string) }
+varOrder = varKey$clean[c(2:nrow(varKey),1)]
+paramTime$var = factor(paramTime$var, levels=varOrder)
+gg=ggplot(paramTime, aes(x=factor(pd), y=mean)) +
 	geom_point() +
-	geom_linerange(aes(ymin=lo90,ymax=hi90), size=.6) +
-	geom_linerange(aes(ymin=lo95,ymax=hi95), size=.2) +
-	geom_hline(aes(yintercept=0)) +
-	facet_wrap(~var, scales='free_y') +
+	geom_linerange(aes(ymin=lo90,ymax=hi90), size=.3) +
+	geom_linerange(aes(ymin=lo95,ymax=hi95), size=.1) +
+	geom_hline(aes(yintercept=0), linetype=2, color = "black") +
+	facet_wrap(~var, scales='free_y', ncol=3,
+		labeller=as_labeller(facet_labeller, default = label_parsed)) +
+	labs(x='', y='') +
 	theme(
 		panel.border = element_blank(),
 		axis.ticks = element_blank(),
-		axis.text.x = element_text(angle=45, hjust=1)
+		axis.text.x = element_text(angle=45, hjust=1, family="Source Sans Pro Light",size=6),
+		axis.text.y=element_text(family="Source Sans Pro Light", hjust=0, size=6),
+		strip.text.x = element_text(size = 9, color='white',
+			family="Source Sans Pro Semibold", 
+			angle=0, hjust=.05),
+		strip.background = element_rect(fill = "#525252", color='#525252')
 		)
+ggsave(gg, file=paste0(pathResults, 'revAppendix/ameTime.pdf'), width=8, height=6, device=cairo_pdf)
+system(paste0('open ',pathResults, 'revAppendix/ameTime.pdf'))
 ################	
