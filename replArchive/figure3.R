0f4b9afffeb45e0563156043db11a7a124e5c41d
s7minhas <s7.minhas@gmail.com>
2018-10-18 08:51:11 -0400

    smplfy repl archive

---
+++ replArchive/figure3.R
@@ -0,0 +1,52 @@
+################
+# workspace
+source('setup.R')
+
+# load fns from helper file
+source('netPlotHelpers.R')
+source('actorInfo.R')
+################
+
+################
+# load data
+load('nigeriaMatList_acled_v7.rda') # loads yList object
+yrs = char(2000:2016) ; yList = yList[yrs]
+################
+
+################
+# clean up row/col names in yList
+vNameKey = getNameKey(yList)
+yList = lapply(yList, function(y){
+	rownames(y) = vNameKey$clean[match(rownames(y),vNameKey$dirty)]
+	colnames(y) = vNameKey$clean[match(colnames(y),vNameKey$dirty)]
+	return(y) })
+################
+
+################
+# set up actor positions across T
+yArr = listToArray(
+	actors=getActor(yList), Y=yList, 
+	Xdyad=NULL, Xrow=NULL, Xcol=NULL)$Y
+yArrSum = getMatfromArr(arr=yArr)
+govActors = c('Military\n(Nigeria)','Police\n(Nigeria)')
+
+# set up pos mat
+tmp=getGraphfromMat(mat=yArrSum)
+gArrSum=tmp$g
+gArrPos=tmp$nodePos
+rm(tmp)
+################
+
+################
+# plot by full, pre and post bh periods
+fName = paste0('figure3.pdf')
+pdf(file=fName, width=13,height=9)
+vCol = ifelse(names(V(gArrSum)) %in% govActors, 'gray30', 'gray95')
+vLabCol = ifelse(names(V(gArrSum)) %in% govActors, 'white', 'gray30')
+plotGraph(gArrSum, gArrPos, 
+	vShape='circle', vLabCex=gArrSum$labSize+.3, 
+	vLabCol=vLabCol, vertex.label.font=1, vertex.size=gArrSum$vSize, 
+	vFrameCol=vCol, vCol=vCol, vertex.label.family="Helvetica")
+title('Nigerian Intra-State Conflict\n(2000-2016)', family='Helvetica', adj=1, line=-4)	
+dev.off()
+################
---
+++ replArchive/figure4.R
@@ -0,0 +1,151 @@
+################
+# workspace
+source('setup.R')
+loadPkg('devtools')
+devtools::install_github('s7minhas/amen', ref='pa2018_version')
+library(amen)
+################
+
+################
+# load data
+load('nigeriaMatList_acled_v7.rda') # loads yList object
+load('exoVars.rda') # load xNodeL, xDyadL
+
+# focus on post 2000 data [few actors beforehand]
+yrs = char(2000:2016)
+yList = yList[yrs] ; xDyadL = xDyadL[yrs] ; xNodeL = xNodeL[yrs]
+###############
+
+################
+# set up model specs
+subListArray = function(lA, vars, dims=2){
+	if(dims==2){ return( lapply(lA, function(x){ x[,vars, drop=FALSE] }) ) }
+	if(dims==3){ return( lapply(lA, function(x){ x[,,vars,drop=FALSE] }) ) } }
+designArrays = list(
+	base=list(
+		senCovar=subListArray(xNodeL, c('riotsProtestsAgainst', 'vioCivEvents', 'groupSpread'), 2),
+		recCovar=subListArray(xNodeL, c('riotsProtestsAgainst', 'vioCivEvents', 'groupSpread'), 2),
+		dyadCovar=subListArray(xDyadL, c('govActor', 'postBoko', 'elecYear', 'ngbrConfCount'), 3)
+		)
+	)
+
+# run base model
+ameFits = lapply(1:length(designArrays), function(i){
+	fit = ame_repL(
+		Y=yList, Xdyad=designArrays[[i]]$dyadCovar,
+		Xrow=designArrays[[i]]$senCovar, Xcol=designArrays[[i]]$recCovar,
+		symmetric=FALSE, rvar=TRUE, cvar=TRUE, R=2, 
+		model='bin', intercept=TRUE, seed=6886,
+		burn=10000, nscan=50000, odens=25,
+		plot=FALSE, gof=TRUE, periodicSave=FALSE )
+	return(fit)
+})
+names(ameFits) = names(designArrays)
+################
+
+################
+# save
+save(
+	ameFits, designArrays,
+	file=paste0(pathResults, 'ameResults.rda')
+	)
+################
+
+################
+# functions to help with viz of results
+source('postHelpers.R')
+source('paramPlot2.R')
+################
+
+################
+# load data
+load('ameResults.rda') # load AME mod results
+
+# quick trace plot
+mcmcData = ameFits$base$BETA
+varKey = data.frame(dirty=colnames(mcmcData),stringsAsFactors=FALSE)
+varKey$clean = c(
+	'Intercept',
+	'Riots/Protests Against (Sender)', 'Civilian Attacks (Sender)', 'Geographic Spread (Sender)',
+	'Riots/Protests Against (Receiver)', 'Civilian Attacks (Receiver)', 'Geographic Spread (Receiver)',
+	'Gov-Gov Actors','Post-Boko Haram', 'Election Year', 'Neighborhood Conflict')
+varKey = varKey[c(9,2,5,3,6,4,7,10,11,8,1),]
+################
+
+################
+# calc standardized reg coefs
+sdY = sd(gfitFullSpec$data$value)
+xVars = colnames(ameFits$base$BETA) %>% gsub('.dyad','',.) %>% .[-1]
+sdX = apply(gfitFullSpec$data[,xVars], 2, sd)
+
+#
+tmp=ameFits$base$BETA[,2:ncol(ameFits$base$BETA)]
+for(i in 1:ncol(tmp)){ tmp[,i] = tmp[,i] * (sdX[i]/sdY)}
+ameFits$base$BETA[,2:ncol(ameFits$base$BETA)] = tmp
+################
+
+################
+# gather beta estimates from ame fullSpec
+ameBETA = t(apply(ameFits$base$BETA, 2, summStats))
+colnames(ameBETA) = c('mean','lo95','hi95','lo90','hi90')
+ameBETA = data.frame(ameBETA, stringsAsFactors = FALSE)
+ameBETA$var = rownames(ameBETA) ; rownames(ameBETA) = NULL
+
+# drop extras and unnecessary params
+ameBETA = ameBETA[-which(ameBETA$var %in% c('intercept')),]
+
+# classify vars by type
+ameBETA$type = unlist(lapply(strsplit(ameBETA$var,'.',fixed=TRUE), function(x){x[2]}))
+ameBETA$var = gsub('.','',gsub('dyad|row|col','',ameBETA$var),fixed=TRUE)
+
+# cleanup
+ameBETA$varClean = c(
+	'Riots/Protests$_{i,t-1}$',
+	'Violent Events\nAgainst Civilians$_{i,t-1}$',
+	'Geographic Spread$_{i,t-1}$',
+	'Riots/Protests$_{j,t-1}$',
+	'Violent Events\nAgainst Civilians$_{j,t-1}$',
+	'Geographic Spread$_{j,t-1}$',
+	'Gov-Gov Actors$_{ij}$',
+	'Post-Boko\nHaram Period$_{t}$',
+	'Election Year$_{t}$',
+	'Neighborhood Conflict$_{t}$'
+	)
+ameBETA$typeClean = c(
+	rep('... Sender-Level Covariate(s)',3),
+	rep('... Receiver-Level Covariate(s)',3),
+	rep('Parameter Estimates for Country-Level Covariate(s)',4)
+	)
+
+# clean up var labels
+ameBETA$typeClean[ameBETA$var=='govActor'] = '... Dyad-Level Covariate(s)'
+
+# add sig info
+ameBETA = getSigVec(ameBETA)
+################
+
+################
+# viz
+cleanVars = ameBETA$varClean
+ameBETA$typeClean = factor(ameBETA$typeClean, levels=unique(ameBETA$typeClean)[c(4,3,1,2)])
+ggCoef=ggplot(ameBETA, aes(x=varClean, y=mean, color=sig)) +
+	facet_wrap(~typeClean,ncol=1,scales='free_y') + 
+	geom_hline(aes(yintercept=0), linetype=2, color = "black") + 
+	geom_point(size=4) + 
+	geom_linerange(aes(ymin=lo90, ymax=hi90),alpha = 1, size = 1.5) + 
+	geom_linerange(aes(ymin=lo95,ymax=hi95),alpha = 1, size = .5) +	
+	scale_colour_manual(values = coefp_colors, guide=FALSE) +
+	scale_x_discrete('', labels=TeX(rev(cleanVars))) +	
+	ylab(TeX('$\\beta_{p} \\times \\frac{\\sigma_{x_{p}}}{\\sigma_{y}}$')) +
+	coord_flip() + 
+	theme(
+		legend.position='none', legend.title=element_blank(),
+		panel.border=element_blank(),
+		axis.ticks=element_blank(),
+		axis.text.y=element_text(hjust=0),
+		strip.text.x = element_text(size = 9, color='white',
+			angle=0, hjust=.95),
+		strip.background = element_rect(fill = "#525252", color='#525252')				
+	)
+ggsave( ggCoef, file='figure4.pdf', width=7, height=6 )
+################
