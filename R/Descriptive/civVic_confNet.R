fe6a3f700414a0e34ddc67d830a76f9f1ea4cbe2
s7minhas <s7.minhas@gmail.com>
2018-11-28 12:09:39 -0500

    plot update

--- R/Descriptive/civVic_confNet.R
+++ R/Descriptive/civVic_confNet.R
@@ -43,11 +43,11 @@ ggVioCiv = ggplot(vioCivCnt, aes(x=year, y=value)) +
 	theme(
 		axis.ticks=element_blank(),
 		panel.border=element_blank(), 
-		axis.text=element_text(family="Source Sans Pro Light",size=8),
+		axis.text=element_text(family="Source Code Pro Light",size=8),
 		strip.text.x = element_text(size = 10, color='white',
-			family="Source Sans Pro Semibold", angle=0, hjust=.05),
+			family="Source Code Pro Semibold", angle=0, hjust=.05),
 		strip.background = element_rect(fill = "#525252", color='#525252')		
-		)
+		)	
 ggsave(ggVioCiv, 
 	width=10, height=5, 
 	file=paste0(pathGraphics, 'civVicConfNet/vioCiv.pdf'), device=cairo_pdf)
@@ -68,6 +68,7 @@ govActors = c('Military\n(Nigeria)','Police\n(Nigeria)')
 
 # set up pos mat
 tmp=getGraphfromMat(mat=yArrSum) ; gArrSum=tmp$g ; gArrPos=tmp$nodePos ; rm(tmp)
+rownames(gArrPos) = rownames(yArrSum)
 
 # create graph object
 g = graph.adjacency(yArrSum, mode='directed', diag=FALSE, weighted=TRUE)
@@ -85,8 +86,8 @@ degBkts = as.numeric(cut(degree(g), seq(0,50,5) ))
 V(g)$vCol=pal[degBkts]
 V(g)$vLabCol=rep('black', length(V(g))) ; V(g)$vLabCol[degBkts>3] = 'white'
 
-# 
-gArrPos = layout.circle(g) ; rownames(gArrPos) = rownames(yArrSum)
+# # 
+# gArrPos = layout.circle(g) ; 
 
 # plot by t
 selT = rep(FALSE, dim(yArr)[3])
@@ -97,22 +98,57 @@ lapply(1:length(yList), function(t){
 		colBrksByDegree=seq(0,12,3),
 		)
 	g = tmp$g ; pos = tmp$nodePos ; rm(tmp)
-	# vCol = ifelse(names(V(g)) %in% govActors, 'gray30', 'gray95')
-	# vLabCol = ifelse(names(V(g)) %in% govActors, 'white', 'gray30')	
-	vCol = 'gray60' ; vLabCol = 'gray60'
-	fName = paste0(pathGraphics, 'civVicConfNet/nigeriaT_', names(yList)[t], '.png')
+	vCol = ifelse(names(V(g)) %in% govActors, 'gray30', 'gray95')
+	vLabCol = ifelse(names(V(g)) %in% govActors, 'white', 'gray30')	
+	# vCol = 'gray60' ; vLabCol = 'gray60'
+	fName = paste0(pathGraphics, 'old/civVicConfNet/nigeriaT_', names(yList)[t], '.png')
 	png(filename=fName, width=500,height=500)
 	plotGraph(g, pos, 
 		eCurve=FALSE, arrowSize=.5,		
 		vShape='circle', 
 		vLabCex=.0001,
 		vertex.size=g$vSize, 
-		# vertex.label.font=1, vertex.label.family="Helvetica", vLabCol=vLabCol, 
+		# vertex.label.font=1, vertex.label.family="Helvetica", 
+		vLabCol=vLabCol, 
 		vertex.label=NA,
-		vFrameCol='black', vCol=vCol,
-		rescale=TRUE,
-		ylim=c(min(gArrPos[,2]), max(gArrPos[,2])),
-		xlim=c(min(gArrPos[,1]), max(gArrPos[,1]))
+		vFrameCol='black', vCol=vCol
+		# rescale=TRUE,
+		# ylim=c(min(gArrPos[,2]), max(gArrPos[,2])),
+		# xlim=c(min(gArrPos[,1]), max(gArrPos[,1]))
+		)
+	# title(names(yList)[t], family='Helvetica', adj=1, line=-2, cex.main=1)
+	dev.off()
+})
+
+# plot by t buckets 
+yrBkts = list(2001:2004, 2005:2008, 2009:2012, 2013:2016)
+lapply(1:length(yrBkts), function(i){
+	#
+	y = getMatfromArr(yArr, pds=char(yrBkts[[i]]))
+	# y = y[-which(rownames(y)=="Ilajes\nMilitia"),-which(rownames(y)=="Ilajes\nMilitia")]
+	print(mean(y, na.rm=TRUE))
+	tmp = getGraphfromMat(
+		mat=y, nodePos=gArrPos,
+		colBrksByDegree=seq(0,12,3),
+		)
+	g = tmp$g ; pos = tmp$nodePos ; rm(tmp)
+	vCol = ifelse(names(V(g)) %in% govActors, 'gray30', 'gray95')
+	vLabCol = ifelse(names(V(g)) %in% govActors, 'white', 'gray30')	
+	# vCol = 'gray60' ; vLabCol = 'gray60'
+	fName = paste0(pathGraphics, 'old/civVicConfNet/nigeriaYrBkt_', i, '.png')
+	png(filename=fName, width=500,height=500)
+	plotGraph(g, pos, 
+		eCurve=FALSE, arrowSize=.5,		
+		vShape='circle', 
+		vLabCex=.0001,
+		vertex.size=g$vSize, 
+		# vertex.label.font=1, vertex.label.family="Helvetica", 
+		vLabCol=vLabCol, 
+		vertex.label=NA,
+		vFrameCol='black', vCol=vCol
+		# rescale=TRUE,
+		# ylim=c(min(gArrPos[,2]), max(gArrPos[,2])),
+		# xlim=c(min(gArrPos[,1]), max(gArrPos[,1]))
 		)
 	# title(names(yList)[t], family='Helvetica', adj=1, line=-2, cex.main=1)
 	dev.off()
